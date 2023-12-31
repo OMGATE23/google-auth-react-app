a3d2b43b279c14239a7d5e27c220b9812b029330
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-12-31 16:20:35 +0530

    feat : header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useAuthContext } from '../hooks/useAuthContext'
+import { useLogout } from '../hooks/useLogout'
+
+const Header = () => {
+    const {user} = useAuthContext()
+    const {logout , isPending , error} = useLogout()
+  return (
+    <>
+        {
+            user && (
+                <header className='p-4 flex justify-end gap-4'>
+                    <button 
+                    disabled = {isPending} 
+                    onClick={logout}
+                    className={`outline outline-1 py-2 px-4 rounded-md ${error ? 'text-red-600' : ""}`}>
+                    {error ? "Error Occured" : "Logout"}    
+                    </button>
+                </header>
+            )
+
+        }
+    </>
+  )
+}
+
+export default Header
