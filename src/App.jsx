c2fabc3a12028f87b136cfaf6053fe278d3f4f0a
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-12-31 16:11:52 +0530

    feat : router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {createBrowserRouter , RouterProvider, redirect} from 'react-router-dom'
+import Main from './layout/Main'
+import LandingPage from './pages/LandingPage'
+import HomePage from './pages/HomePage'
+import { useAuthContext } from './hooks/useAuthContext'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {authIsReady , user} = useAuthContext()
+  const router = createBrowserRouter([
+    {
+      path : "/",
+      element : <Main/>,
+      errorElement : <h1>Oops , Something went wrong!</h1> ,
+      children : [
+        {
+          index : true,
+          element : <LandingPage/>,
+          loader : () => {
+            if(user){
+              return redirect('/home')
+            }
+            return null
+          }
+          
+        },
+        {
+          path : 'home',
+          element : <HomePage/>,
+          loader : () => {
+            if(!user){
+              return redirect('/')
+            }
+            return null
+          }
+        }
+      ]
+    }
+  ])
   return (
     <>
-      <h1 className=''>I am here</h1>
+      {authIsReady && (
+        <RouterProvider router={router} />
+      )}
     </>
   )
 }
